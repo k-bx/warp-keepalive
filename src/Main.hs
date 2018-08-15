3daf3b9120ce5d09280db0964ae1989eabe7ef3a
Kostiantyn Rybnikov <k-bx@k-bx.com>
2018-08-15 20:44:52 +0300

    Try using the keep-alive

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
+
+import Control.Exception (bracket_)
+import Network.HTTP.Types.Status
+import Network.Wai
+import qualified Network.Wai.Handler.Warp as W
+import qualified Network.Wai.Handler.WarpTLS as W
+
+app :: Application
+app req respond =
+  bracket_
+    (putStrLn "Allocating scarce resource")
+    (putStrLn "Cleaning up")
+    (respond $ responseLBS status200 [("Connection", "keep-alive")] "Hello World")
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  W.runTLS (W.tlsSettings "certificate.pem" "key.pem") W.defaultSettings app
